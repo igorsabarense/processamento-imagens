47d184746ee05cfe822f203f8ee034967e80d922
Igor Sabarense <igor.sabarense@gmail.com>
2021-10-07 22:51:29 -0300

    # achando cada digito + retangulo

--- ocr-project.py
+++ ocr-project.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import imutils
+from PIL import Image
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QImage, QPixmap, QPalette, QPainter, QTransform
 from PyQt5.QtPrintSupport import QPrintDialog, QPrinter
@@ -15,6 +16,18 @@ import numpy as np
     __date__ = "2021"
 """
 
+DIGITS_LOOKUP = {
+    (1, 1, 1, 0, 1, 1, 1): 0,
+    (0, 0, 1, 0, 0, 1, 0): 1,
+    (1, 0, 1, 1, 1, 1, 0): 2,
+    (1, 0, 1, 1, 0, 1, 1): 3,
+    (0, 1, 1, 1, 0, 1, 0): 4,
+    (1, 1, 0, 1, 0, 1, 1): 5,
+    (1, 1, 0, 1, 1, 1, 1): 6,
+    (1, 0, 1, 0, 0, 1, 0): 7,
+    (1, 1, 1, 1, 1, 1, 1): 8,
+    (1, 1, 1, 1, 0, 1, 1): 9
+}
 
 class QImageViewer(QMainWindow):
     def __init__(self):
@@ -56,6 +69,7 @@ class QImageViewer(QMainWindow):
             self.cv_imagem = cv2.imread(nomeArquivo)
 
             image = self.retornaQImage()
+            self.contornoImagem()
 
             if image.isNull():
                 QMessageBox.information(self, "Visualizador", "Não foi possível carregar %s." % nomeArquivo)
@@ -186,7 +200,88 @@ class QImageViewer(QMainWindow):
         barraRolagem.setValue(int(escala * barraRolagem.value()
                                   + ((escala - 1) * barraRolagem.pageStep() / 2)))
 
+    def contornoImagem(self):
+       image = self.cv_imagem.copy()
+       gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+       thresh = cv2.threshold(gray, 0, 255,
+                              cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+       kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 5))
 
+
+       # find contours in the thresholded image, then initialize the
+       # digit contours lists
+       cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+       cnts = cnts[0]
+
+       digitCnts = []
+
+       # loop over the digit area candidates
+       for c in cnts:
+           # compute the bounding box of the contour
+           (x, y, w, h) = cv2.boundingRect(c)
+
+           # if the contour is sufficiently large, it must be a digit
+           print('w,h ='), w, h
+           cv2.rectangle(self.cv_imagem, (x, y), (x + w, y + h), (0, 255, 0), 1)
+           if w >= 15 and (h >= 30 and h <= 70):
+               digitCnts.append(c)
+
+       # sort the contours from left-to-right, then initialize the
+       # actual digits themselves
+       # digitCnts = contours.sort_contours(digitCnts,
+       #	method="left-to-right")[0]
+       digits = []
+
+       # loop over each of the digits
+       print('digitCnts'), digitCnts
+       for c in digitCnts:
+           # extract the digit ROI
+           (x, y, w, h) = cv2.boundingRect(c)
+           roi = thresh[y:y + h, x:x + w]
+
+           # compute the width and height of each of the 7 segments
+           # we are going to examine
+           (roiH, roiW) = roi.shape
+           (dW, dH) = (int(roiW * 0.25), int(roiH * 0.15))
+           dHC = int(roiH * 0.05)
+
+           # define the set of 7 segments
+           segments = [
+               ((0, 0), (w, dH)),  # top
+               ((0, 0), (dW, h // 2)),  # top-left
+               ((w - dW, 0), (w, h // 2)),  # top-right
+               ((0, (h // 2) - dHC), (w, (h // 2) + dHC)),  # center
+               ((0, h // 2), (dW, h)),  # bottom-left
+               ((w - dW, h // 2), (w, h)),  # bottom-right
+               ((0, h - dH), (w, h))  # bottom
+           ]
+           on = [0] * len(segments)
+
+           # sort the contours from left-to-right, then initialize the
+           # actual digits themselves
+           # loop over the segments
+           for (i, ((xA, yA), (xB, yB))) in enumerate(segments):
+               # extract the segment ROI, count the total number of
+               # thresholded pixels in the segment, and then compute
+               # the area of the segment
+               segROI = roi[yA:yB, xA:xB]
+               total = cv2.countNonZero(segROI)
+               area = (xB - xA) * (yB - yA)
+
+               # if the total number of non-zero pixels is greater than
+               # 50% of the area, mark the segment as "on"
+               if total / float(area) > 0.5:
+                   on[i] = 1
+
+           # lookup the digit and draw it on the image
+           digit = DIGITS_LOOKUP[tuple(on)]
+           digits.append(digit)
+           cv2.rectangle(self.cv_imagem, (x, y), (x + w, y + h), (0, 255, 0), 1)
+           cv2.putText(self.cv_imagem, str(digit), (x - 10, y - 10),
+                       cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 255, 0), 2)
+
+       # display the digits
+       print(digits)
 
 
 
