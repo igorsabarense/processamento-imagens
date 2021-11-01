0489ff82e722de29885af1f42abfcfd4aa04a879
Igor Sabarense <igor.sabarense@gmail.com>
2021-11-01 20:08:04 -0300

    # comment

--- processing_utils.py
+++ processing_utils.py
@@ -97,15 +97,15 @@ def moments(image):
               :param image : npArray
               :return mu_vector, covariance_matrix
     """
-    c0, c1 = np.mgrid[:image.shape[0], :image.shape[1]]  # A trick in numPy to create a mesh grid
-    totalImage = np.sum(image)  # sum of pixels
-    m0 = np.sum(c0 * image) / totalImage  # mu_x
-    m1 = np.sum(c1 * image) / totalImage  # mu_y
-    m00 = np.sum((c0 - m0) ** 2 * image) / totalImage  # var(x)
-    m11 = np.sum((c1 - m1) ** 2 * image) / totalImage  # var(y)
-    m01 = np.sum((c0 - m0) * (c1 - m1) * image) / totalImage  # covariance(x,y)
-    mu_vector = np.array([m0, m1])  # Notice that these are \mu_x, \mu_y respectively
-    covariance_matrix = np.array([[m00, m01], [m01, m11]])  # Do you see a similarity between the covariance matrix
+    c0, c1 = np.mgrid[:image.shape[0], :image.shape[1]]
+    totalImage = np.sum(image)
+    m0 = np.sum(c0 * image) / totalImage
+    m1 = np.sum(c1 * image) / totalImage
+    m00 = np.sum((c0 - m0) ** 2 * image) / totalImage
+    m11 = np.sum((c1 - m1) ** 2 * image) / totalImage
+    m01 = np.sum((c0 - m0) * (c1 - m1) * image) / totalImage
+    mu_vector = np.array([m0, m1])
+    covariance_matrix = np.array([[m00, m01], [m01, m11]])
     return mu_vector, covariance_matrix
 
 
